3a7e5783f32b2474d962bc43d9490b4b742d5d7a
mikeycodingstuff <108337844+mikeycodingstuff@users.noreply.github.com>
2023-02-01 16:12:01 +0000

    added support for arguments

--- weather.py
+++ weather.py
@@ -10,6 +10,21 @@ def read_cli_args():
         description="gets weather and temp information for a given city"
     )
 
+    # Define the "city" argument that’ll take one or many inputs separated by whitespace
+    parser.add_argument(
+        "city",
+        nargs="+",
+        type=str,
+        help="enter the city name"
+    )
+
+    parser.add_argument(
+        "-f",
+        "--fahrenheit",
+        action="store_true",
+        help="display the temperature in fahrenheit",
+    )
+
     return parser.parse_args()
 
 if __name__ == "__main__":

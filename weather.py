b5dd59796e454bbba5247f167a2336164d3826c3
mikeycodingstuff <108337844+mikeycodingstuff@users.noreply.github.com>
2023-02-01 17:10:07 +0000

    used pprint module to display data more cleanly

--- weather.py
+++ weather.py
@@ -1,8 +1,10 @@
 import argparse
 import json
 import sys
+
 from configparser import ConfigParser
 from urllib import error, parse, request
+from pprint import pp
 
 BASE_WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5/weather'
 
@@ -102,4 +104,4 @@ if __name__ == "__main__":
     user_args = read_user_cli_args()
     query_url = build_weather_query(user_args.city, user_args.imperial)
     weather_data = get_weather_data(query_url)
-    print(weather_data)
+    pp(weather_data)

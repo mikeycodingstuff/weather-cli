ae801ed47668f67bfd8931fd8dc19a7b56636f8b
Mikey Ying <mikeyinguk@gmail.com>
2023-02-01 16:29:28 +0000

    added _get_api_key function

--- weather.py
+++ weather.py
@@ -1,4 +1,17 @@
 import argparse
+from configparser import ConfigParser
+
+def _get_api_key():
+    """Fetch the API key from your configuration file.
+
+    Expects a configuration file named "secrets.ini" with structure:
+
+        [openweather]
+        api_key=<YOUR-OPENWEATHER-API-KEY>
+    """
+    config = ConfigParser()
+    config.read("secrets.ini")
+    return config["openweather"]["api_key"]
 
 def read_cli_args():
     """Handles the CLI user interactions.

c1e8c15cc9045ba3670b113c991601bf965c99d8
mikeycodingstuff <108337844+mikeycodingstuff@users.noreply.github.com>
2023-02-01 17:00:52 +0000

    updated try/except to handle different http code errors

--- weather.py
+++ weather.py
@@ -83,11 +83,17 @@ def get_weather_data(query_url):
 
     try:
         response = request.urlopen(query_url)
-    except error.HTTPError:
-        sys.exit("Can't find weather data for this city")
+    except error.HTTPError as http_error:
+        if http_error.code == 401:
+            sys.exit("Access denited. Check your API key")
+        elif http_error.code == 404:
+            sys.exit("Can't find weather data for this city")
+        else:
+            sys.exit(f"Something went wrong... ({http_error.code})")
+            
 
     data = response.read()
-    
+
     return json.loads(data)
 
 if __name__ == "__main__":
